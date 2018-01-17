0e97f74066726ba3b2221f240c07338546b4d913
karnival <david@calpol.us>
2018-01-17 19:48:03 +0000

    Add query parameters to header
    Still not implemented, but now defined

--- geocoder/geocoder.h
+++ geocoder/geocoder.h
@@ -10,15 +10,38 @@ class Geocoder {
 
 		std::string geocode(std::string query);
 		std::string get_format_string();
-
-		bool is_premium = false;
-		bool no_annotations = true;
-
+		
 		enum class OutputFormat {JSON, GEOJSON, XML, MAP, GOOGLE_JSON}; 
 		OutputFormat output_format = OutputFormat::JSON;
 
+		// Boolean query parameters always used, defaults as when unspecified.
+		bool no_annotations = true;
+		bool abbrv = false;
+		bool no_dedupe = false;
+		bool no_record = false;
+		bool pretty = false;
+
+		// Setters for non-default parameters.
+		void set_min_confidence(int confidence);
+		void set_bounds(float bounds[4]);
+		void set_countrycode(std::string code);
+		void set_language(std::string langugage);
+
 	private:
 		std::string api_key;
+
+		// Query parameters not used by default.
+		bool use_min_confidence = false;
+		bool use_bounds = false;
+		bool use_countrycode = false;
+		bool use_language = false;
+
+		std::string language = "";
+		int limit = 0;
+		int min_confidence = 0;
+		float bounds[4] = {0.0, 0.0, 0.0, 0.0};
+		std::string countrycode = "";
+
 };
 
 #endif
